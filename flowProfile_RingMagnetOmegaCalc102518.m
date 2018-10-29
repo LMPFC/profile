55300c10d34e61c4dd7913eea79e9f03389e1d83
LMPFC <afisher@sunfire14.pppl.gov>
2018-10-29 16:17:31 -0400

    Added in a comment about the code being added to github

--- flowProfile_RingMagnetOmegaCalc102518.m
+++ flowProfile_RingMagnetOmegaCalc102518.m
@@ -5,6 +5,8 @@ close all
 % Adding capabilities as of 10/25/18 12:28:28PM for looping over multiple
 % directories created by pipeMeshForRingMagCalc.m
 
+% Made into git repository 10/29/18 ~4PM.
+
 dirList = dir('flowProfileMeshes');
 numOfFolders = size(dirList,1);
 
